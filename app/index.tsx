3e156417b309241be86e3e4cefb0c8cce444d523
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-08-27 15:35:51 +0800

    Set Up NativeWind with Types

--- app/index.tsx
+++ app/index.tsx
@@ -3,16 +3,9 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>This is it! Show must go on!</Text>
+    <View className="flex-1 justify-center items-center bg-teal-500">
+      <Text className="text-2xl">This is it! Show must go on!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center'
-    }
-});
