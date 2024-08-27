93eaf3e44b9e8e834c3309d086344126eea3859c
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-08-27 16:37:10 +0800

    initial Screen Set Up

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,40 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, ImageBackground } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+import {LinearGradient} from 'expo-linear-gradient';
+import {StatusBar} from "expo-status-bar";
+
+import beachImg from "@/assets/meditation-images/beach.webp";
 
 
-export default function HomeScreen() {
+const App = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-teal-500">
-      <Text className="text-2xl">This is it! Show must go on!</Text>
+    <View className='flex-1'>
+      <ImageBackground 
+      source={beachImg} 
+      resizeMode="cover" 
+      className='flex-1'
+      >
+        <LinearGradient 
+          className="flex-1" 
+          colors={["rgba(0,0,0,0.4)","rgba(0,0,0,0.8)"]}
+          >
+          <SafeAreaView className="flex-1 justify-between mx-5 my-14 px-1 ">
+            <View>
+              <Text className='text-center text-white font-semibold text-4xl'>Just Relax</Text>
+              <Text className='text-center text-white text-xl'>Simplifying meditation to everyone!</Text>
+            </View>
+            <View>
+              <CustomButton 
+                title="Get Started"
+                onPress={()=> console.log('button test')}
+                />
+            </View>
+              <StatusBar style="light"/>
+          </SafeAreaView>
+        </LinearGradient>  
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
+export default App;
